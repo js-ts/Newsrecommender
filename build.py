c68341f269b7eda13fd8cabbe78cd37aa6a34b31
Zia Muhammad <zia-muhammad@outlook.com>
2019-01-05 20:05:49 +0100

    Add flask server

--- build.py
+++ build.py
@@ -1,4 +1,4 @@
-from pybuilder.core import use_plugin, init
+from pybuilder.core import use_plugin, init, Author
 
 use_plugin("python.core")
 use_plugin("python.unittest")
@@ -8,16 +8,24 @@ use_plugin("python.coverage")
 use_plugin("python.distutils")
 
 
-name = "newsrecommender"
-default_task = "publish"
+name    = "newsrecommender"
+version = "1.0"
 
+summary = "News recommender"
+url     = "https://github.com/Ziasafi/newsrecommender.git"
+
+description = """An example PyBuilder / Git project for project management
+and file version control. See blog post at http://bit.ly/2QY65wO for a
+more through explanation."""
+
+authors      = [Author("Zia Muhammad", "zia-muhammad@outlook.com")]
+license      = "None"
+default_task = "publish"
 
 @init
 def initialize(project):
     project.build_depends_on("mockito")
 
-
 @init
 def set_properties(project):
     pass
-
---
+++ target/dist/newsrecommender-1.0/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from setuptools.command.install import install as _install
+
+class install(_install):
+    def pre_install_script(self):
+        pass
+
+    def post_install_script(self):
+        pass
+
+    def run(self):
+        self.pre_install_script()
+
+        _install.run(self)
+
+        self.post_install_script()
+
+if __name__ == '__main__':
+    setup(
+        name = 'newsrecommender',
+        version = '1.0',
+        description = 'News recommender',
+        long_description = 'An example PyBuilder / Git project for project management\nand file version control. See blog post at http://bit.ly/2QY65wO for a\nmore through explanation.',
+        author = 'Zia Muhammad',
+        author_email = 'zia-muhammad@outlook.com',
+        license = 'None',
+        url = 'https://github.com/Ziasafi/newsrecommender.git',
+        scripts = [],
+        packages = [],
+        namespace_packages = [],
+        py_modules = [
+            'article_to_json',
+            'demo',
+            'news_articles_processor'
+        ],
+        classifiers = [
+            'Development Status :: 3 - Alpha',
+            'Programming Language :: Python'
+        ],
+        entry_points = {},
+        data_files = [],
+        package_data = {},
+        install_requires = [],
+        dependency_links = [],
+        zip_safe = True,
+        cmdclass = {'install': install},
+        keywords = '',
+        python_requires = '',
+        obsoletes = [],
+    )
