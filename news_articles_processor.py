a08fb8a9471a8187dc219e5a3f93d0d0d87653c0
Zia <zia-muhammad@outlook.com>
2019-01-05 19:51:51 +0100

    Add high order functions

--- news_articles_processor.py
+++ news_articles_processor.py
@@ -1,5 +1,6 @@
 import nltk
 import requests
+import re
 import json
 from bs4 import BeautifulSoup
 from nltk.tokenize import word_tokenize 
@@ -20,6 +21,7 @@ def get_parsed_text(text):
 	text = get_remove_text(soup)
 	return text
 
+
 def get_remove_text(soup):
 	
 	for script in soup(["script", "style"]):
@@ -33,14 +35,21 @@ def get_remove_text(soup):
 
 def get_tokens(text):
 	text_tokens = nltk.tokenize.word_tokenize(text)		
-	return text_tokens
+#	return text_tokens
+	def get_parts_of_speech():
+		pos_tagged = nltk.pos_tag(text_tokens)
+		return pos_tagged
+	return get_parts_of_speech
 
-def get_parts_of_speach(tokens_list):
-	tagged = nltk.pos_tag(tokens_list)
-	return tagged
 
-def get_nouns(tokens_list):
-	tagged = nltk.pos_tag(tokens_list)
+#def get_parts_of_speech(tokens_list):
+#	tagged = nltk.pos_tag(tokens_list)
+	#pos_tagged = list(map(get_parts_of_speach, tagged))
+#	return tagged
+
+def get_nouns(tagged):
+	#tagged = nltk.pos_tag(text_tokens)
+	#tagged = get_part_of_speach(text_token)
 	list_noun_tokens = []
 	 
 	for token, tag_token in tagged:
@@ -57,8 +66,8 @@ def write_article_to_json(articles_list):
 	for url in articles_list:
 		text = get_article_content(url)
 		list_tokens = get_tokens(text)
-		list_pos = get_parts_of_speach(list_tokens)
-		list_nouns = get_nouns(list_tokens)
+		list_pos = get_parts_of_speech(text_tokens)
+		list_nouns = get_nouns(text_tokens)
 		dict_data = {
 		 	"url"		: url,
 		 	"text"		: text,
@@ -111,8 +120,9 @@ def sort_by_value(tosort_dict):
 def get_top_3_articles(url):
 	article_list_from_json = read_article_from_json()
 	text = get_article_content(url)
-	list_tokens = get_tokens(text)
-	list_nouns = get_nouns(list_tokens)
+	pos_function = get_tokens(text)
+	list_of_pos = pos_function() 
+	list_nouns = get_nouns(list_of_pos)
 	common_words_dict = {}
 	for article in article_list_from_json:
 		common_words = get_common_words(
@@ -124,3 +134,8 @@ def get_top_3_articles(url):
 	top_3_match = sorted_dict[-3:]
 	top_3_match = list(reversed(top_3_match))
 	return top_3_match
+
+
+if __name__ == '__main__':
+	pos_tags = get_tokens('khan is angry because little Khan can not remember things from 5 minutes earlier.')
+	print(pos_tags())
