3df84adcb62fb7bbd049b0638a37860578767212
Zia Muhammad <zia-muhammad@outlook.com>
2019-01-09 22:37:40 +0100

    Update path unittest file

---
+++ src/unittest/python/test_news_articles_processor.py
@@ -0,0 +1,16 @@
+import unittest
+from news_articles_processor import get_tokens
+from news_articles_processor import get_nouns
+
+
+class TestGetToken(unittest.TestCase):
+
+	def test_get_token(self):
+		self.assertNotEqual(len(get_tokens('hello world')), 0)
+
+	def test_get_nouns(self):
+		self.assertEqual(get_nouns(['car', 'banana', 'go']), ['car', 'banana'])
+
+
+if __name__ == '__main__':
+	unittest.main()
